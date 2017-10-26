c8321cedbe87eb87781e2b8be75907081f00d5c0
apaulin <apaulin@users.noreply.github.com>
2017-10-26 09:15:03 -0400

    Update webd.js
    New brags

--- webd.js
+++ webd.js
@@ -16,14 +16,20 @@ var players = null;
 var transporter = null;
 
 var endPhrases = new Array(
-	"I think I have been hacked.  Those can't possibly be your choices.  Someone must really wants to put a joke on you to submit these to your name!",
-	"I am looking at these choices and I cannot stop laughing!!!",
-	"Maybe next year you should enroll in a badminton Pickem!!",
-	"Based on these choices, I wouldn't watch TV on Sunday if I were you...",
-	"You can change these picks until Thursday noon.  I strongly recommend that you do!",
-	"Mppffffftttt, mppffffftttt, mppffffftttt  POUHAHAHAHAHAHAAHA!!!!!  Those are f-u-n-n-y!!!!!\nThank you for that, my day was boring so far.",
-	"Are you picking your teams because you like their shirt color?  Sure looks like it...",
-	"Reminder, the goal of this game is to finish on top.  Looking at your picks, I figured you didn't know that."
+	//"I think I have been hacked.  Those can't possibly be your choices.  Someone must really wants to put a joke on you to submit these to your name!",
+	//"I am looking at these choices and I cannot stop laughing!!!",
+	//"Maybe next year you should enroll in a badminton Pickem!!",
+	//"Based on these choices, I wouldn't watch TV on Sunday if I were you...",
+	//"You can change these picks until Thursday noon.  I strongly recommend that you do!",
+	//"Mppffffftttt, mppffffftttt, mppffffftttt  POUHAHAHAHAHAHAAHA!!!!!  Those are f-u-n-n-y!!!!!\nThank you for that, my day was boring so far.",
+	//"Are you picking your teams because you like their shirt color?  Sure looks like it...",
+	//"Reminder, the goal of this game is to finish on top.  Looking at your picks, I figured you didn't know that.",
+	"You could at least make the effort of looking at the screen while making your picks!",
+	"I hope you at least have a great personality...",
+	"If your place in society was based based on these choices, you would have to go to jail.  These are a crime against football!!!!!!",
+	"Up until now the worst decision in gambling history was made by Pete Rose, you sadly beat him by a long stretch!",
+	"Maybe I should make an 'Insta-pick' feature just for you...",
+	"Geeeeeese... If you don't want to play anymore, just quit!" 
 
 );
 
